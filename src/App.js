03a496954406fc2359c3ab2f9ab8d285f359348b
Matheria <sensenevercame@gmail.com>
2023-05-26 19:25:50 +0500

    feat: add products array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import Drawer from './components/Drawer';
 import Card from './components/Card/Card';
 
 function App() {
+  const arr = [
+    {
+      title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+      price: 12999,
+      imageUrl: '/img/sneakers/1.jpg',
+    },
+    { title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg' },
+    {
+      title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+      price: 8499,
+      imageUrl: '/img/sneakers/3.jpg',
+    },
+    {
+      title: 'Кроссовки Puma X Aka Boku Future Rider',
+      price: 8999,
+      imageUrl: '/img/sneakers/4.jpg',
+    },
+  ];
+
   return (
     <div className="wrapper clear">
       <Drawer />
