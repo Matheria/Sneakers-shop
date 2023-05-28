64b1f0f11ac53481f61011c83a46a62cef43c4c1
Matheria <sensenevercame@gmail.com>
2023-05-28 13:57:37 +0500

    feat: add images

--- src/App.js
+++ src/App.js
@@ -7,18 +7,62 @@ function App() {
     {
       title: 'Мужские Кроссовки Nike Blazer Mid Suede',
       price: 12999,
-      imageUrl: '/img/sneakers/1.jpg',
+      imageUrl: '/img/sneakers/NikeBlazerMidSuede.jpg',
     },
-    { title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg' },
+    {
+      title: 'Мужские Кроссовки Nike Air Max 270',
+      price: 8999,
+      imageUrl: '/img/sneakers/NikeAirMax270.jpg',
+    },
     {
       title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-      price: 8499,
-      imageUrl: '/img/sneakers/3.jpg',
+      price: 12499,
+      imageUrl: '/img/sneakers/NikeBlazerMidSuede.jpg',
     },
     {
       title: 'Кроссовки Puma X Aka Boku Future Rider',
       price: 8999,
-      imageUrl: '/img/sneakers/4.jpg',
+      imageUrl: '/img/sneakers/PumaXAkaBokuFutureRider.jpg',
+    },
+    {
+      title: 'Мужские Кроссовки Under Armour Curry 8',
+      price: 15999,
+      imageUrl: '/img/sneakers/UnderArmourCurry8.jpg',
+    },
+    {
+      title: 'Мужские Кроссовки Nike Kyrie 7',
+      price: 11999,
+      imageUrl: '/img/sneakers/NikeKyrie7.jpg',
+    },
+    {
+      title: 'Мужские Кроссовки Jordan Air Jordan 11',
+      price: 10999,
+      imageUrl: '/img/sneakers/JordanAirJordan11.jpg',
+    },
+    {
+      title: 'Мужские Кроссовки Nike LeBron XVIII',
+      price: 16499,
+      imageUrl: '/img/sneakers/NikeLeBronXVIII.jpg',
+    },
+    {
+      title: 'Мужские Кроссовки Nike Lebron XVIII Low',
+      price: 13499,
+      imageUrl: '/img/sneakers/NikeLebronXVIIILowLow.jpg',
+    },
+    {
+      title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+      price: 8499,
+      imageUrl: '/img/sneakers/NikeBlazerMidSuede.jpg',
+    },
+    {
+      title: 'Кроссовки Puma X Aka Boku Future Rider',
+      price: 8999,
+      imageUrl: '/img/sneakers/PumaXAkaBokuFutureRider.jpg',
+    },
+    {
+      title: 'Мужские Кроссовки Nike Kyrie Flytrap IV',
+      price: 11499,
+      imageUrl: '/img/sneakers/NikeKyrieFlytrapIV.jpg',
     },
   ];
 
